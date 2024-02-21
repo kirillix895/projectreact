a9b56589a68b6d28e05104962ed123b9b8142521
Kirill <kirillklopov8954408@gmail.com>
2024-02-21 15:46:13 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+"use strict";
+//alert("hello")
+//let s = 25;
+//const pov = {
+//    a:50
+//}
+//console.log(pov.a);
+//const result = confirm("Are you here?");
+//console.log(result);
+//const answer = +prompt("вам есть 18?","18");
+//console.log(answer);
+//console.log(typeof(answer));
+//const answers = [];
+//answers[0]= prompt("как ваше имя?","");
+//answers[1]= prompt("как ваша фамилия?","");
+//answers[2]= prompt("сколько вам лет?","");
+ //document.write(answers);
+ const category = 'toys';
+ console.log(`https://someurl.com/${category}/5`)
+ const user = "Ivan";
+ alert(`Привет,${user}`);
+ console.log(typeof('arr'+"- object"));
+ console.log(4 + +"6");
+ let incr = 10,
+     decr = 10;
+// ++incr;
+ //--decr;
+ console.log(++incr);   
+ console.log(--decr);  
+//console.log(incr); 
