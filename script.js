eacccd7e68d472ed92b36a212f877f9ae852213d
Kirill <kirillklopov8954408@gmail.com>
2024-03-19 14:11:59 +0300

    первые задания

--- script.js
+++ script.js
@@ -28,3 +28,21 @@
  console.log(++incr);   
  console.log(--decr);  
 //console.log(incr); 
+const numberOfFilms=+prompt("сколько фильмов вы уже посмотрели?","");
+console.log(numberOfFilms[0]);
+
+const personalMovieDB = {
+    count:numberOfFilms,
+    movies:{
+    },
+    actors:{},
+    genres:[],
+    privat:false
+};
+const a = prompt("один из последних просмотренных фильмов?",""),
+      b = prompt("на сколько оцените его?",""),
+      c = prompt("один из последних просмотренных фильмов?",""),
+      d = prompt("на сколько оцените его?","");
+personalMovieDB.movies[a]=b;
+personalMovieDB.movies[c]=d;
+console.log(personalMovieDB)
